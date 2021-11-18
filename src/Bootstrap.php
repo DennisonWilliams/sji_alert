06a16335b97de6d16744b3fc4c2b62d28ad02a2a
Dennison Williams <DennisonWilliams@gmail.com>
2021-11-18 14:21:52 -0800

    This is functional, but the form should probably be rolled up into the module as well

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -14,19 +14,19 @@
  */
 
 namespace OpenEMR\Modules\SJIAlert;
+//require_once("interface/globals.php");
 
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Note the below use statements are importing classes from the OpenEMR core codebase
  */
-use OpenEMR\Menu\MenuEvent;
-use OpenEMR\Events\RestApiExtend\RestApiCreateEvent;
+use OpenEMR\Events\PatientDemographics\RenderEvent;
 
 class Bootstrap
 {
-    const MODULE_INSTALLATION_PATH = "";
-    const MODULE_NAME = "sji-alert";
+	const MODULE_INSTALLATION_PATH = "";
+	const MODULE_NAME = "sji-alert";
 	/**
 	 * @var EventDispatcherInterface The object responsible for sending and subscribing to events through the OpenEMR system
 	 */
@@ -34,7 +34,7 @@ class Bootstrap
 
 	public function __construct(EventDispatcherInterface $eventDispatcher)
 	{
-	    $this->eventDispatcher = $eventDispatcher;
+		$this->eventDispatcher = $eventDispatcher;
 	}
 
 	public function subscribeToEvents()
@@ -44,9 +44,39 @@ class Bootstrap
 
 	public function addSJIAlert(RenderEvent $event) {
 		$pid = $event->getPid();
+		$db = $GLOBALS['adodb']['db'];
+
+		$sql = 
+		"SELECT form_sji_alert.date as date,form_sji_alert.alert as alert FROM form_sji_alert " .
+		"LEFT JOIN forms on (form_sji_alert.id=forms.form_id) ".
+		"WHERE form_sji_alert.pid = " .  $db->qstr($pid) ." ".
+		"AND forms.formdir='sji_alert' ".
+		"AND forms.deleted=0";
+		$result = $db->Execute($sql);
+		if ($db->ErrorMsg()) {
+		    return error_log($db->ErrorMsg());
+		}
 
 		// TODO: If there are any active alerts for the participant 
 		// then we should click on a link to the alert pop up with js
+		while ($result && !$result->EOF) {
+			echo "<a href='/interface/modules/custom_modules/sji_alert/src/alert.php?".
+			"pid=". attr_url($pid) .
+			"' id='alert_popup' ".
+			"style='display: none;' onclick='top.restoreSession()'></a>\n".
+
+			// show the active alert modal
+			'<script type="text/javascript">'."\n".
+			"dlgopen('', 'aleretreminder', 300, 170, '', false, { ".
+			"allowResize: false, ".
+			"allowDrag: true, ".
+			"dialogId: '', ".
+			"type: 'iframe', ".
+			'url: $("#alert_popup").attr("href") '.
+			"});".
+			'</script>'."\n";
+			break;
+		}        
 	}
 
 }
